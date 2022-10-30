53ac7ee067ad59b56284278f88715e587f927ca1
Anıl Haydar Yılmaz <anilhyilmaz@gmail.com>
2022-10-31 02:54:47 +0300

    fixed a bug and game screen design

--- lib/Screens/GridviewBuilder.dart
+++ lib/Screens/GridviewBuilder.dart
@@ -9,36 +9,55 @@ class GridviewBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var finished = Provider.of<Repo>(context, listen: false).isFinished;
 
-    var finished = Provider.of<Repo>(context,listen: false).isFinished;
-
-
-    return Scaffold(appBar: AppBar(title: Text("Game Screen")),body: Center(
-      child: Container(
-        height: 300,
-        width: 300,
-        margin: const EdgeInsets.all(10),
-        child: GridView.builder(gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 100,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 20),itemCount: Provider.of<Repo>(context).mylist.length,itemBuilder: (context,index){
-          return GestureDetector(
-            onTap: ()=>{
-              finished ? print("game finished") : Provider.of<Repo>(context,listen: false).changeText(index),
-            },
-            child: Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: Colors.amber,
-                  borderRadius: BorderRadius.circular(15)),
-              child: Text(Provider.of<Repo>(context).mylist[index] ?? " ",style: TextStyle(fontSize: constOsize.toDouble())),
+    return Scaffold(
+        appBar: AppBar(title: Text("Game Screen")),
+        body: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              flex: 1,
+              child: Column(children: [Text("Username1: ggg"),Text("Username2: fff")],),
             ),
-          );
-        },),
-      ),
-    ));
+            Flexible(
+              flex: 4,
+              child: Container(
+                height: 300,
+                width: 300,
+                margin: const EdgeInsets.all(10),
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 100,
+                      childAspectRatio: 3 / 2,
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 20),
+                  itemCount: Provider.of<Repo>(context).mylist.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () => {
+                        finished
+                            ? print("game finished")
+                            : Provider.of<Repo>(context, listen: false)
+                                .changeText(index),
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                            Provider.of<Repo>(context).mylist[index] ?? " ",
+                            style: TextStyle(fontSize: constOsize.toDouble())),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        )));
   }
 }
-
-
